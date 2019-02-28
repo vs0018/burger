1a84a7630a2bd6fe9c1a8df6236609f3ea6514d8
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-02-28 18:52:04 -0500

    minor change to route names

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -7,7 +7,7 @@ var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-  burger.insertOne([
+  burger.create([
     "burger_name", "devoured"
   ], [
     req.body.burger_name, req.body.devoured
@@ -32,7 +32,7 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.updateOne({
+  burger.update({
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
