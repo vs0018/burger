bfa30836a2f62e8c60cba3fac8d9b7a3c672f665
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-03-02 00:23:12 -0500

    jawsDB changes

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,21 @@
 // Set up MySQL connection.
+require("dotenv").config();
+
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Charleston85*",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.SQL_PASS,
+    database: "burgers_db"
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
